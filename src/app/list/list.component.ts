1fb2189bb1f758ef650b495ea4e34f1e8c1e9d34
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-16 21:21:31 +0700

    het buoi 1

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -3,11 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-list',
   template: `
-    <div>LIST</div>
-  `,
-  styleUrls: ['./list.component.css']
+    <img [src]="imageSrc" />
+    <div style="padding: 5px">
+      <div *ngFor="let word of words">
+        <h3>{{ word.en }}</h3>
+        <p>{{ word.vn }}</p>
+      </div>
+    </div>
+  `
 })
 export class ListComponent {
-
-  constructor() { }
+  imageSrc = 'https://www.google.com.vn/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png';
+  words = [
+    { en: 'hello', vn: 'xin chao' },
+    { en: 'space', vn: 'khoang cach' },
+    { en: 'line', vn: 'dong' },
+  ];
 }
