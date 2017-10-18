6ea608e93ea51ab868be5f31112cb2e1f5e8c2ff
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-18 20:16:29 +0700

    my-input

--- src/app/my-input/my-input.component.ts
+++ src/app/my-input/my-input.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-input',
-  templateUrl: './my-input.component.html',
-  styleUrls: ['./my-input.component.css']
+  templateUrl: './my-input.component.html'
 })
-export class MyInputComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class MyInputComponent {
 
 }
